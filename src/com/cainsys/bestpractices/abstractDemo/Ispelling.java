09075d0d4e17703900bf4a5b2837622a56e8dc08
praveenchitambaram <praveen.chitambaram@chainsys.com>
2022-06-08 15:59:17 +0530

    added packages

--- src/com/cainsys/bestpractices/abstractDemo/Ispelling.java
+++
@@ -1,8 +0,0 @@
-package com.cainsys.bestpractices.abstractDemo;
-
-public interface Ispelling {
-	void spellCheck(String word);
-	void checkGrammer(String sentence);
-	int wordCount(String sentence);
-
-}
---
+++ src/com/chainsys/bestPractices/abstractDemo/ScoreBoard.java
@@ -0,0 +1,13 @@
+package com.chainsys.bestPractices.abstractDemo;
+
+public class ScoreBoard {
+	public int TargetScore = 215;
+
+	private ScoreBoard() {
+		System.out.println("Object created for ScoreBoard");
+	}
+
+	public static ScoreBoard createObject() {
+		return new ScoreBoard();
+	}
+}
